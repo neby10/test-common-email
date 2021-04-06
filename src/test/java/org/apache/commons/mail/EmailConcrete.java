9dccc3774566503e13e6df0a679fdd6a934bdc39
Nicholas Eby <49201079+neby10@users.noreply.github.com>
2021-04-06 19:56:26 -0400

    Upload Initial Files - src
    Getting project ready with the src folder

---
+++ src/test/java/org/apache/commons/mail/EmailConcrete.java
@@ -0,0 +1,22 @@
+package org.apache.commons.mail;
+
+import java.util.Map;
+
+
+public class EmailConcrete extends Email {
+	@Override
+	public Email setMsg(String msg) throws EmailException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	public Map<String, String> getHeaders()
+	{
+		return this.headers;
+	}
+	public String getContentType()
+	{
+		return this.contentType;
+	}
+}
