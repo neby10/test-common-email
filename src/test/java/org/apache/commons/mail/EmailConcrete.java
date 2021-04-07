bace3b864a7e97cd2a0cffc67f4e4ead2713950e
Nicholas Eby <ng83eby@yahoo.com>
2021-04-06 20:51:40 -0400

    changes to EmailConcrete.java made for testing. Map, String, and @Override of setMsg

--- src/test/java/org/apache/commons/mail/EmailConcrete.java
+++ src/test/java/org/apache/commons/mail/EmailConcrete.java
@@ -3,20 +3,21 @@ package org.apache.commons.mail;
 import java.util.Map;
 
 
+
 public class EmailConcrete extends Email {
-	@Override
-	public Email setMsg(String msg) throws EmailException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
-	public Map<String, String> getHeaders()
-	{
-		return this.headers;
-	}
-	public String getContentType()
-	{
-		return this.contentType;
-	}
+    @Override
+    public Email setMsg(String msg) throws EmailException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    
+    public Map<String, String> getHeaders()
+    {
+        return this.headers;
+    }
+    public String getContentType()
+    {
+        return this.contentType;
+    }
 }
